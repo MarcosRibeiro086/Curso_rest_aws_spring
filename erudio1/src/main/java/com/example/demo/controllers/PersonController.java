ebb794dd2a03685db095289427e63f1311845654
Marcos <marcosvictor82@gmail.com>
2024-08-13 01:49:59 -0300

    melhorando annotations, criando mensagem de erro personalizada e implementando lógica no service

--- erudio1/src/main/java/com/example/demo/controllers/PersonController.java
+++ erudio1/src/main/java/com/example/demo/controllers/PersonController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 //import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,10 +17,31 @@ public class PersonController {
 	@Autowired
 	private PersonServices service;
 	
-	@RequestMapping(value="/{id}",method=RequestMethod.GET,produces=MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value="id")String id) throws Exception{
+	@GetMapping(value="/{id}",produces=MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value="id")Long id) {
 		return service.findById(id);
 	}
 	
+	@GetMapping(produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll(){
+		return service.findAll();
+	}
+	
+	@PostMapping(consumes=MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) {
+		return service.create(person);
+	}
+	
+	@PutMapping(consumes=MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) {
+		return service.update(person);
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<?>delete(@PathVariable(value="id")Long id) {
+		 service.delete(id);
+		 return ResponseEntity.noContent().build();
+	}
+
 
 }
--- erudio1/src/main/java/com/example/demo/service/PersonServices.java
+++ erudio1/src/main/java/com/example/demo/service/PersonServices.java
@@ -1,29 +1,63 @@
 package com.example.demo.service;
 
+import java.util.*;
 import java.util.concurrent.atomic.*;
 import java.util.logging.Logger;
-import com.example.demo.model.Person;
 
+import com.example.demo.exceptions.ResourceNotFoundException;
+import com.example.demo.model.Person;
+import com.example.demo.repositories.PersonRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 //import com.example.demo.model.Person;
 
 @Service
 public class PersonServices {
-	private final AtomicLong counter = new AtomicLong();
 	private Logger logger=Logger.getLogger(PersonServices.class.getName());
 	
+	@Autowired
+	PersonRepository repository;
 	
-	public Person findById(String id) {
-		logger.info("finding one person");
-		Person person=new Person();
+	public List<Person> findAll(){
 		
-		person.setId(counter.incrementAndGet());
-		person.setFirstName("Marcos");
-		person.setLastName("Ribeiro");
-		person.setAddress("Parnamirim - RN - Brasil");
-		person.setGender("male");
-		return person;
-	}
+		logger.info("finding all people!");
 
+		return repository.findAll();
+	}
+	
+	public Person create (Person person) {
+		logger.info("Creating one person!");
+		return repository.save(person);
+	}
+	
+	
+	public Person update (Person person) {
+		logger.info("Updating one person!");
+		
+		//recupera por id jogo na dentro da variável para modificar o objeto escolhido
+		var entity=repository.findById(person.getId()).orElseThrow(()-> new ResourceNotFoundException("No records found for this ID!"));
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		return repository.save(person);
+	}
+	
+	public void delete (Long id) {
+		logger.info("Deleting one person");
+		//recuperando a entidade a ser deletada
+		var entity=repository.findById(id).orElseThrow(()-> new ResourceNotFoundException("No records found for this ID!"));
+		
+		repository.delete(entity);
+	}
+	
+	public Person findById(Long id) {
+		logger.info("finding one person");
+
+		return repository.findById(id).orElseThrow(()-> new ResourceNotFoundException("No records found for this ID!"));
+	}
+	
+	
 }
